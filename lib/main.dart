d83ced18ae9233e4beef1f04873bb0f949ae6876
arshia <khaniarshia7@gmail.com>
2025-06-02 16:22:17 +0330

    Add theme controller and implement dark theme support in main.dart
    - Integrated ThemeController for managing app themes.
    - Added dark theme configuration to GetMaterialApp.
    - Updated theme mode to reflect the current theme from ThemeController.

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:castlink/routes/app_pages.dart';
+import 'package:castlink/app/controllers/theme_controller.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+  Get.put(ThemeController());
   runApp(const MyApp());
 }
 
@@ -15,6 +17,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeController themeController = Get.find<ThemeController>();
+    
     return GetMaterialApp(
       title: 'CastLink',
       debugShowCheckedModeBanner: false,
@@ -22,6 +26,14 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: themeController.theme,
       initialRoute: Routes.SPLASH,
       getPages: AppPages.routes,
       initialBinding: SplashBinding()
